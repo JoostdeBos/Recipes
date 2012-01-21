68084478990a1e69b14bb5a68edca71d3d93a87b
Jurre Stender <jurrestender@hotmail.com>
2012-01-21 16:48:01 +0100

    show recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ActiveRecord::Base
 	belongs_to :post
 	has_many :ingredients
  	has_many :items, :through => :ingredients
+ 	accepts_nested_attributes_for :ingredients
+ 	accepts_nested_attributes_for :items
 end
