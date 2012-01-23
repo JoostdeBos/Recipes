a577071106c3db4eba24eadb8077eab866a00453
Jurre Stender <jurrestender@hotmail.com>
2012-01-23 10:08:30 +0100

    profile picture for twitter/facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 	private
 
 	def current_user
-	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+	  @current_user ||= User.find(session[:user_id]) if session[:user_id] unless User.count == 0 # Unless should probably be removed as this never happens when there are users, this is only useful in production
 	end
 end
