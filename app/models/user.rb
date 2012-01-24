43ea8b780e4e4f7f7cd7a69426478e43f666f3d1
Jurre Stender <jurrestender@hotmail.com>
2012-01-24 20:57:22 +0100

    set a default image for users, picture_url needs to be replaced in some places

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :comments
 
+	# Loads a default image for the user if none has been set. Use this method instead of picture_url
+	def image_url
+		self.picture_url.nil? ? 'image-unknown.jpg' : self.picture_url
+	end
+
 	def self.from_omniauth(auth)
 		find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
 	end
