cdaae0c4d01a9896532aa1e2a437d27f350c16a8
Jurre Stender <jurrestender@hotmail.com>
2012-01-24 09:55:31 +0100

    recipe validations

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem "formtastic"
 
 gem "formtastic-bootstrap", "~> 1.1.2"
 
+gem 'client_side_validations'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,9 @@ class Recipe < ActiveRecord::Base
  	has_many :items, :through => :ingredients
  	has_many :comments
  	accepts_nested_attributes_for :ingredients
+
+ 	validates :name, :presence => true
+ 	validates :instructions, :presence => :true
+ 	validates :preperation_time, :presence => :true
+ 	validates :preperation_time, :numericality => { :only_integer => true }
 end
